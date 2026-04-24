2245dca7cb7c9451f65031b8f791e5ff585907b3
AdamFARTOUT1 <adamfartout8@gmail.com>
2026-04-24 21:25:56 +0100

    Étape 3 : Création des Models et définitions des relations

--- app/Models/Planning.php
+++ app/Models/Planning.php
@@ -6,4 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Planning extends Model
 {
+    public function etudiant()  { return $this->belongsTo(Etudiant::class); }
+    public function encadrant() { return $this->belongsTo(Professeur::class, 'encadrant_id'); }
+    public function jury2()     { return $this->belongsTo(Professeur::class, 'jury2_id'); }
+    public function jury3()     { return $this->belongsTo(Professeur::class, 'jury3_id'); }
+    public function creneau()   { return $this->belongsTo(Creneau::class);}
+    public function salle()     { return $this->belongsTo(Salle::class);}
 }
--- app/Models/Professeur.php
+++ app/Models/Professeur.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professeur extends Model
 {
-    //
+    public function etudiantsEncadres() { return $this->hasMany(Etudiant::class);
+}
 }
